c77a19a8600b000a66be8134254ee208059641f8
Chris Kowalik <chris@nu7hat.ch>
2013-07-02 15:01:24 -0300

    fixed interface docs

--- histogram.go
+++ histogram.go
@@ -1,12 +1,12 @@
 package gohistogram
 
 // Histogram is the interface that wraps the Add and Quantile methods.
-//
-// Add adds a new value, n, to the histogram. Trimming is done automatically.
-//
-// Quantile returns an approximation.
 type Histogram interface {
+	// Add adds a new value, n, to the histogram. Trimming is done
+	// automatically.
 	Add(n float64)
+
+	// Quantile returns an approximation.
 	Quantile(n float64) (q float64)
 }
 
